531823d0c510752a79c928f90bc8126346cadf69
Mohammad Tarek <tarek3529@gmail.com>
2025-02-23 13:58:09 +0200

    working on brand page

--- brand-page.php
+++ brand-page.php
@@ -6,12 +6,13 @@ Template Post Type: post, page, brand
 
 get_header();
 
-// Get the brand mobile, whatsapp, telegram, and description
+// Get the brand custom fields
+$brand_logo = get_the_post_thumbnail_url(get_the_ID(), 'full');
+$brand_background_image = get_field('background_image');
+$brand_description = get_field('description');
 $brand_mobile = get_field('mobile');
 $brand_whatsapp = get_field('whatsapp');
 $brand_telegram = get_field('telegram');
-$brand_description = get_field('description');
-$brand_background_image = get_field('background_image');
 
 $brand_mobile = ensure_a_plus_two($brand_mobile);
 $brand_whatsapp = ensure_a_plus_two($brand_whatsapp);
@@ -39,6 +40,7 @@ while (have_posts()) : the_post();
   ));
 
   $number_of_products_on_sale = count_products_on_sale($related_products);
+  $number_of_products_with_videos = count_products_with_videos($related_products);
 
 endwhile;
 ?>
@@ -56,10 +58,15 @@ endwhile;
 
     <?php
     if ($brand_background_image) {
-      echo '<div class="absolute inset-0 bg-black opacity-40"></div>';
+      echo '<div class="absolute inset-0 bg-black opacity-60"></div>';
     }
     ?>
 
+    <?php if ($brand_logo) : ?>
+      <div class="z-10">
+        <img class="rounded-full aspect-square object-cover" width="100px" src="<?php echo $brand_logo; ?>" alt="">
+      </div>
+    <?php endif; ?>
     <h1 class="<?php echo ($brand_background_image ? 'text-white' : '') ?> text-3xl md:text-4xl lg:text-6xl my-4 font-bold z-10"><?php the_title(); ?></h1>
     <p class="<?php echo ($brand_background_image ? 'text-white' : '') ?> text-lg md:text-xl lg:text-2xl z-10">
       <?php echo $brand_description; ?>
@@ -94,25 +101,29 @@ endwhile;
     </div>
 
     <!-- Tabs -->
-    <div class="brand-tabs w-full h-16 bg-gray-200 grid grid-cols-3 text-base md:text-lg lg:text-xl my-12">
+    <div class="brand-tabs w-full h-16 flex justify-center text-base md:text-lg lg:text-xl my-12">
       <a
         href="?tab=products"
-        class="flex justify-center items-center h-full cursor-pointer hover:bg-custom-blue hover:text-white">
+        class="flex-grow flex justify-center items-center h-full cursor-pointer bg-gray-200 hover:bg-custom-blue hover:text-white">
         <?php echo __("All Products", "my-theme-child"); ?>
       </a>
-      <a
-        href="?tab=videos"
-        class="flex justify-center items-center h-full cursor-pointer hover:bg-custom-blue hover:text-white">
-        <?php echo __("Videos", "my-theme-child"); ?>
-      </a>
-      <a
-        href="?tab=sales"
-        class="flex justify-center items-center h-full cursor-pointer hover:bg-custom-blue hover:text-white">
-        <?php echo __("Sales", "my-theme-child"); ?>
-      </a>
+      <?php if ($number_of_products_with_videos > 0) : ?>
+        <a
+          href="?tab=videos"
+          class="flex-grow flex justify-center items-center h-full cursor-pointer bg-gray-200 hover:bg-custom-blue hover:text-white">
+          <?php echo __("Videos", "my-theme-child"); ?>
+        </a>
+      <?php endif; ?>
+      <?php if ($number_of_products_on_sale > 0) : ?>
+        <a
+          href="?tab=sale"
+          class="flex-grow flex justify-center items-center h-full cursor-pointer bg-gray-200 hover:bg-custom-blue hover:text-white">
+          <?php echo __("Sale", "my-theme-child"); ?>
+        </a>
+      <?php endif; ?>
     </div>
 
-    <!-- Products|Videos|Sales -->
+    <!-- Products|Videos|Sale -->
     <?php
     // Output videos and product details
     if ($related_products->have_posts()) : ?>
@@ -125,13 +136,10 @@ endwhile;
         <?php display_product_videos($related_products); ?>
       </div>
 
-      <div class="astra-container brand-sales" style="display: none;">
+      <div class="astra-container brand-sale" style="display: none;">
         <?php display_products($related_products, true); ?>
       </div>
     <?php endif; ?>
-
-
-
 
     <div class="brand-info">
       <div class="brand-description">
