bf3e2595e21159dd86ce4125b1c56f9f9f5b7259
Mohammad Tarek <tarek3529@gmail.com>
2025-03-08 20:53:52 +0200

    feat: add brand display template in blog content

--- template-parts/content-blog.php
+++ template-parts/content-blog.php
@@ -15,6 +15,18 @@
 // if post is a product, display the product card
 if (get_post_type() === 'product') {
   get_template_part('template-parts/product', 'card');
+} else if (get_post_type() === 'brand') {
+  $brand_image = get_the_post_thumbnail_url(get_the_ID(), 'full');
+  $brand_short_description = get_field('description');
+  ?>
+  <a class="px-4" href="<?php the_permalink(); ?>">
+    <div class="brand-item p-4 flex flex-col items-center justify-center gap-4 border border-gray-200 rounded-md">
+      <img class="w-[250px] h-[150px] rounded-full object-cover" src="<?php echo $brand_image; ?>" alt="<?php the_title(); ?>">
+      <h3 class="text-center text-lg font-bold"><?php the_title(); ?></h3>
+      <p class="text-center"><?php echo $brand_short_description; ?></p>
+    </div>
+  </a>
+<?php
 } else {
 ?>
   <article
