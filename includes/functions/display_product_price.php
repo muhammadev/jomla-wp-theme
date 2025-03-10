194abf9e21a7af56f1b546809061e721df70663d
Mohammad Tarek <tarek3529@gmail.com>
2025-03-11 00:38:18 +0200

    fix: ensure original price is displayed only if available in product price function

--- includes/functions/display_product_price.php
+++ includes/functions/display_product_price.php
@@ -2,21 +2,25 @@
 
 function display_product_price()
 {
+  $original_price = get_field("price");
 ?>
-  <!-- if there's an offer, display the price after offer, if not, display the original price -->
-  <?php if (get_field('offer')) : ?>
-    <p>
-      <span class="text-base md:text-lg lg:text-xl text-red-500">
-        <?php the_field('price_after_offer'); ?>&nbsp;<?php echo esc_html__('EGP', 'my-theme-child') ?>
-      </span>
-      <span class="text-sm md:text-md lg:text-lg">
-        <?php echo esc_html__('Instead of', 'my-theme-child') ?>
-        <span class="line-through">
-          <?php the_field('price'); ?>&nbsp;<?php echo esc_html__('EGP', 'my-theme-child') ?>
+  <?php if ($original_price) : ?>
+    <!-- if there's an offer, display the price after offer, if not, display the original price -->
+    <?php if (get_field('offer')) : ?>
+      <p>
+        <span class="text-base md:text-lg lg:text-xl text-red-500">
+          <?php the_field('price_after_offer'); ?>&nbsp;<?php echo esc_html__('EGP', 'my-theme-child') ?>
         </span>
-      </span>
-    </p>
-  <?php else : ?>
-    <p class="text-base md:text-lg lg:text-xl"><?php the_field('price'); ?>&nbsp;<?php echo esc_html__('EGP', 'my-theme-child') ?></p>
-<?php endif;
+        <span class="text-sm md:text-md lg:text-lg">
+          <?php echo esc_html__('Instead of', 'my-theme-child') ?>
+          <span class="line-through">
+            <?php the_field('price'); ?>&nbsp;<?php echo esc_html__('EGP', 'my-theme-child') ?>
+          </span>
+        </span>
+      </p>
+    <?php else : ?>
+      <p class="text-base md:text-lg lg:text-xl"><?php the_field('price'); ?>&nbsp;<?php echo esc_html__('EGP', 'my-theme-child') ?></p>
+<?php
+    endif;
+  endif;
 }
