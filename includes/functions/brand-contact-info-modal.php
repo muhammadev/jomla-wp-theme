06ce0348e25e2047452a5a794769e3fb38ad30b0
Mohammad Tarek <tarek3529@gmail.com>
2025-03-10 04:34:37 +0200

    fix: conditionally display brand contact methods in modal

--- includes/functions/brand-contact-info-modal.php
+++ includes/functions/brand-contact-info-modal.php
@@ -20,33 +20,38 @@ function brand_contact_info_modal($brand_id)
       <p class="my-5"><?php printf(esc_html__('You can contact %s via the following channels:', 'my-theme-child'), $brand_title) ?></p>
 
       <div class="grid grid-cols-1 gap-4 items-center text-base md:text-lg lg:text-xl">
-        <!-- mobile -->
-        <div class="flex items-center gap-4 col-span-1">
-          <span class="flex items-center gap-2 text-lg font-bold">
-            <i class="inline-block w-8"><img src="<?php echo get_stylesheet_directory_uri() ?>/src/assets/imgs/phone.svg" alt=""></i>
-            <?php echo esc_html__('Mobile:', 'my-theme-child') ?>
-          </span>
-        </div>
-        <a id="brand-mobile" dir="ltr" href="tel:<?php echo $brand_mobile ?>" class="bg-custom-blue text-center text-white hover:text-white p-4 rounded"><?php echo $brand_mobile ?></a>
+        <?php if ($brand_mobile) : ?>
+          <!-- mobile -->
+          <div class="flex items-center gap-4 col-span-1">
+            <span class="flex items-center gap-2 text-lg font-bold">
+              <i class="inline-block w-8"><img src="<?php echo get_stylesheet_directory_uri() ?>/src/assets/imgs/phone.svg" alt=""></i>
+              <?php echo esc_html__('Mobile:', 'my-theme-child') ?>
+            </span>
+          </div>
+          <a id="brand-mobile" dir="ltr" href="tel:<?php echo $brand_mobile ?>" class="bg-custom-blue text-center text-white hover:text-white p-4 rounded"><?php echo $brand_mobile ?></a>
+        <?php endif; ?>
 
-        <!-- WhatsApp -->
-        <div class="flex items-center gap-4">
-          <span class="flex items-center gap-2 text-lg font-bold">
-            <i class="inline-block w-8"><img src="<?php echo get_stylesheet_directory_uri() ?>/src/assets/imgs/wa.svg" alt=""></i>
-            <?php echo esc_html__('WhatsApp:', 'my-theme-child') ?>
-          </span>
-        </div>
-        <a id="brand-whatsapp" dir="ltr" href="https://wa.me/<?php echo $brand_whatsapp ?>" class="bg-custom-blue text-center text-white hover:text-white p-4 rounded"><?php echo $brand_whatsapp ?></a>
+        <?php if ($brand_whatsapp) : ?>
+          <!-- WhatsApp -->
+          <div class="flex items-center gap-4">
+            <span class="flex items-center gap-2 text-lg font-bold">
+              <i class="inline-block w-8"><img src="<?php echo get_stylesheet_directory_uri() ?>/src/assets/imgs/wa.svg" alt=""></i>
+              <?php echo esc_html__('WhatsApp:', 'my-theme-child') ?>
+            </span>
+          </div>
+          <a id="brand-whatsapp" dir="ltr" href="https://wa.me/<?php echo $brand_whatsapp ?>" class="bg-custom-blue text-center text-white hover:text-white p-4 rounded"><?php echo $brand_whatsapp ?></a>
+        <?php endif; ?>
 
-        <!-- Telegram -->
-        <div class="flex items-center gap-4">
-          <span class="flex items-center gap-2 text-lg font-bold">
-            <i class="inline-block w-8"><img src="<?php echo get_stylesheet_directory_uri() ?>/src/assets/imgs/telegram.svg" alt=""></i>
-            <?php echo esc_html__('Telegram:', 'my-theme-child') ?>
-          </span>
-        </div>
-        <a id="brand-telegram" dir="ltr" href="https://t.me/<?php echo $brand_telegram ?>" class="bg-custom-blue text-center text-white hover:text-white p-4 rounded"><?php echo $brand_telegram ?></a>
-
+        <?php if ($brand_telegram) : ?>
+          <!-- Telegram -->
+          <div class="flex items-center gap-4">
+            <span class="flex items-center gap-2 text-lg font-bold">
+              <i class="inline-block w-8"><img src="<?php echo get_stylesheet_directory_uri() ?>/src/assets/imgs/telegram.svg" alt=""></i>
+              <?php echo esc_html__('Telegram:', 'my-theme-child') ?>
+            </span>
+          </div>
+          <a id="brand-telegram" dir="ltr" href="https://t.me/<?php echo $brand_telegram ?>" class="bg-custom-blue text-center text-white hover:text-white p-4 rounded"><?php echo $brand_telegram ?></a>
+        <?php endif; ?>
       </div>
 
       <button class="close text-white px-4 py-2 rounded-lg mt-5">
