c85ce45f801a5e2026afd1f97a5c34f3ce6c4c6c
Mohammad Tarek <tarek3529@gmail.com>
2025-03-10 04:34:45 +0200

    fix: add conditional check for featured products slider initialization

--- src/js/home.js
+++ src/js/home.js
@@ -2,23 +2,27 @@ jQuery(document).ready(function ($) {
   const isRTL = $("html").attr("dir") === "rtl";
   window.isRTL = isRTL;
 
-  $(".featured-products-slider").slick({
-    rtl: isRTL,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: true,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          dots: true,
+  const productSlider = $(".featured-products-slider");
+
+  if (productSlider.length) {
+    productSlider.slick({
+      rtl: isRTL,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: true,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            dots: true,
+          },
         },
-      },
-    ],
-    autoplay: true,
-    autoplaySpeed: 5000,
-    pauseOnHover: false,
-    pauseOnFocus: true,
-    pauseOnDotsHover: true,
-  });
+      ],
+      autoplay: true,
+      autoplaySpeed: 5000,
+      pauseOnHover: false,
+      pauseOnFocus: true,
+      pauseOnDotsHover: true,
+    });
+  }
 });
